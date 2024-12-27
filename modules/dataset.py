4ab76d15c2825c793a75a1e234abecdf5bdf76a6
Peyker Akbulut <peykererkol@gmail.com>
2024-12-27 18:18:38 +0300

    Fixed issue on about dataset

--- modules/dataset.py
+++ modules/dataset.py
@@ -2,13 +2,27 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 
-# Başlık
-st.title("About Dataset")
+def dataset_page():
+    # Başlık
+    st.title("About Dataset")
+    
+    # Container genişliğini artır
+    st.markdown("""
+        <style>
+        .block-container {
+            max-width: 1200px;
+            padding-top: 2rem;
+            padding-right: 2rem;
+            padding-left: 2rem;
+            padding-bottom: 2rem;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
-# HTML dosyasını oku
-with open('static/customer_churn_report.html', 'r', encoding='utf-8') as file:
-    html_content = file.read()
+    # HTML dosyasını oku
+    with open('static/customer_churn_report.html', 'r', encoding='utf-8') as file:
+        html_content = file.read()
 
-# HTML'i doğrudan components.html ile göster
-components.html(html_content, height=800, scrolling=True)
+    # HTML'i doğrudan components.html ile göster
+    components.html(html_content, height=800, scrolling=True)
 
