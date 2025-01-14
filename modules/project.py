9b88caf0e27b19105180b30339db3ddce11253d2
Peyker Akbulut <peykererkol@gmail.com>
2025-01-14 14:54:01 +0300

    Updated 2 column for homepage

--- modules/project.py
+++ modules/project.py
@@ -4,9 +4,24 @@ def home_page():
     # Başlık
     st.title('Bank Customer Churn Prediction APP')
 
-    # İçerik
-    st.markdown("- Customer loyalty is of critical importance for banks and financial institutions. Customer churn not only results in revenue loss but also incurs higher costs for acquiring new customers. Therefore, predicting the likelihood of customers leaving the bank and developing preventive strategies is an essential step to enhance the competitive edge of banks.")
-    st.markdown("- In this project, we aim to predict customer churn rates in banks using machine learning techniques. By analyzing past customer behaviors and their usage levels of the bank's services, we strive to identify which customers are more likely to leave the bank.")
-    st.image("images/data_processing.png", width=500)
+    # İki kolonlu düzen oluştur
+    col1, col2 = st.columns([3, 2])  # 3:2 oranında kolonlar
+
+    with col1:
+        # Metin içeriği
+        st.markdown("""
+        Customer loyalty is of critical importance for banks and financial institutions. 
+        Customer churn not only results in revenue loss but also incurs higher costs for acquiring new customers. 
+        Therefore, predicting the likelihood of customers leaving the bank and developing preventive strategies 
+        is an essential step to enhance the competitive edge of banks.
+        
+        In this project, we aim to predict customer churn rates in banks using machine learning techniques. 
+        By analyzing past customer behaviors and their usage levels of the bank's services, 
+        we strive to identify which customers are more likely to leave the bank.
+        """)
+
+    with col2:
+        # Resim
+        st.image("images/data-cleaning.png", use_container_width=True)
 
 # Fonksiyonu direkt çağırmayalım, home.py'dan çağıracağız
